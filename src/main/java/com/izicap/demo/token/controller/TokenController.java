9159c239ea855705d7e543fe01779e2147069db5
Jacques Belissent <jbelis@gmail.com>
2021-02-24 18:35:55 +0100

    fix token create response

--- src/main/java/com/izicap/demo/token/controller/TokenController.java
+++ src/main/java/com/izicap/demo/token/controller/TokenController.java
@@ -3,11 +3,16 @@ package com.izicap.demo.token.controller;
 
 import com.izicap.demo.token.controller.dtos.TokenResponse;
 import com.izicap.demo.token.controller.dtos.UserAndPassword;
+import com.izicap.demo.token.entities.Token;
 import com.izicap.demo.token.services.TokenService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
@@ -18,10 +23,8 @@ public class TokenController {
     TokenService tokenService;
 
     @RequestMapping(value = "", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
-    @ResponseStatus(HttpStatus.CREATED)
-    public TokenResponse getOrCreateToken(@RequestBody UserAndPassword userAndPassword) {
-
-        return new TokenResponse(tokenService.getOrCreateToken(userAndPassword.getUsername(), userAndPassword.getPassword()));
+    public ResponseEntity<TokenResponse> getOrCreateToken(@RequestBody UserAndPassword userAndPassword) {
+        Token t = tokenService.getOrCreateToken(userAndPassword.getUsername(), userAndPassword.getPassword());
+        return new ResponseEntity<>(new TokenResponse(t), t.isCreated() ? HttpStatus.CREATED : HttpStatus.OK);
     }
-
 }
